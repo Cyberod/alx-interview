5c3c3a6868a049db42d219df9434922b4fbb6383
Cyberod <110993874+Cyberod@users.noreply.github.com>
2023-06-30 15:50:07 +0100

    Update 0-pascal_triangle.py

--- 0x00-pascal_triangle/0-pascal_triangle.py
+++ 0x00-pascal_triangle/0-pascal_triangle.py
@@ -1,17 +1,9 @@
 #!/usr/bin/python3
 """ Pascal triangle """
-
 def pascal_triangle(n):
-	"""
-	Function that prints pascal's triangle
-	"""
-	if n <= 0:
-		return []
-
     list1 = []
     for i in range(n):
         temp_list = []
-		
         for j in range(i+1):
             if j == 0 or j == i:
                 temp_list.append(1)
@@ -19,4 +11,4 @@ def pascal_triangle(n):
                 temp_list.append(list1[i-1][j-1] + list1[i-1][j])
         list1.append(temp_list)
 
-	return list1
+    return list1

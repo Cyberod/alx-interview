c4a04de365428809fb251d28c800120ec927ae84
Cyberod <110993874+Cyberod@users.noreply.github.com>
2023-06-30 15:46:00 +0100

    Pascal Triangle

--- 0x00-pascal_triangle/0-pascal_triangle.py
+++ 0x00-pascal_triangle/0-pascal_triangle.py
@@ -8,17 +8,13 @@ def pascal_triangle(n):
 	if n <= 0:
 		return []
 
-	""" initialize an empty resulting array """
     list1 = []
     for i in range(n):
-		""" initialize an empty temporary array """
         temp_list = []
         for j in range(i+1):
-			""" the first and last column is always set to 1 """
             if j == 0 or j == i:
                 temp_list.append(1)
             else:
-				""" Add each consecutive column numbers of the previous row """
                 temp_list.append(list1[i-1][j-1] + list1[i-1][j])
         list1.append(temp_list)
 

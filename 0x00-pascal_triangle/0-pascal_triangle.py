c1670f6fcb6e40ac67aa928e129c97f2d9812df5
Cyberod <110993874+Cyberod@users.noreply.github.com>
2023-06-30 15:48:51 +0100

    Update 0-pascal_triangle.py

--- 0x00-pascal_triangle/0-pascal_triangle.py
+++ 0x00-pascal_triangle/0-pascal_triangle.py
@@ -11,6 +11,7 @@ def pascal_triangle(n):
     list1 = []
     for i in range(n):
         temp_list = []
+		
         for j in range(i+1):
             if j == 0 or j == i:
                 temp_list.append(1)
